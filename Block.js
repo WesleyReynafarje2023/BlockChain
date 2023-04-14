9d9ae6286efe265d76aab0ad63f6b26104872357
Wesley Reynafarje <wesley.reynafarje@mrjaffesclass.com>
2023-04-14 12:54:04 -0700

    finsihed with 1

--- Block.js
+++ Block.js
@@ -13,4 +13,15 @@ class Block {
     getHash() {
         return SHA256(this.prevHash + this.timestamp + JSON.stringify(this.data));
     }
+
+    mine(difficulty) {
+        // Basically, it loops until our hash starts with 
+        // the string 0...000 with length of <difficulty>.
+        while (!this.hash.startsWith(Array(difficulty + 1).join("0"))) {
+            // We increases our nonce so that we can get a whole different hash.
+            this.nonce++;
+            // Update our new hash with the new nonce value.
+            this.hash = this.getHash();
+        }
+    }
 }
